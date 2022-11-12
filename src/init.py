e74f58269c1365174707df95c308b33e82ea325f
Timothy Nguyen <timhnguyen98@gmail.com>
2022-11-13 05:24:18 +1100

    updated init.py

--- src/init.py
+++ src/init.py
@@ -3,11 +3,7 @@ from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
-#Create instance of SQLAlchemy
 db = SQLAlchemy()
-#Create instance of Marshmallow
 ma = Marshmallow()
-#Create instance of Bcrypt
 bcrypt = Bcrypt()
-#Create instance of JWT
 jwt = JWTManager()
