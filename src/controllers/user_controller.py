a2978999fd729a890a06cda63c4763fd23c994d7
Timothy Nguyen <timhnguyen98@gmail.com>
2022-11-13 05:52:15 +1100

    added user controller

---
+++ src/controllers/user_controller.py
@@ -0,0 +1,48 @@
+from flask import Blueprint, request
+from init import db, bcrypt
+from models.user import User, UserSchema
+from controllers.auth_controller import authorize_user
+from flask_jwt_extended import get_jwt_identity, jwt_required
+
+users_bp = Blueprint('users', __name__, url_prefix='/users')
+
+@users_bp.route('/')
+@jwt_required()
+def get_all_users():
+    authorize_user()
+    stmt = db.select(User).order_by(User.id)
+    users = db.session.scalars(stmt)
+    return UserSchema(many=True, exclude=['password']).dump(users)
+
+@users_bp.route('/<int:id>/')
+@jwt_required()
+def get_one_user(id):
+    # Allows a user to get another user details
+    stmt = db.select(User).filter_by(id=id)
+    user = db.session.scalar(stmt)
+
+    if user:
+        return UserSchema(exclude=['password', 'email']).dump(user)
+
+    return {'error': f'User not found with id {id}'}, 404
+
+@users_bp.route('/update/', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_user():
+    # User update their name, password, email
+    stmt = db.select(User).filter_by(id=get_jwt_identity())
+    user = db.session.scalar(stmt)
+
+    data =  UserSchema().load(request.json, partial=True)
+
+    if user:
+        user.name = data.get('name') or user.name
+        user.email = data.get('email') or user.email
+        if data.get('password'):
+            user.password = bcrypt.generate_password_hash(request.json.get('password')).decode('utf-8')
+        db.session.commit()
+        return {
+            "Message": f"User successfully updated",
+            "User": UserSchema(exclude=['password']).dump(user)
+        }
+    return {'error': f'User not found with id {id}'}, 404
