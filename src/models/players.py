6ad71f64aa9ae3d3726fc0ca6ab8e01152ec77eb
Timothy Nguyen <timhnguyen98@gmail.com>
2022-11-13 05:27:09 +1100

    added player model

--- src/models/players.py
+++ src/models/players.py
@@ -1,19 +1,25 @@
 from init import db, ma
 from marshmallow import fields
 
-class User(db.Model):
-    __tablename__ = 'users'
+class Player(db.Model):
+    __tablename__ = 'players'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    email = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)
+    team = db.Column(db.String)
+    position = db.Column(db.String)
+    points = db.Column(db.Integer)
+    rebounds = db.Column(db.Integer)
+    assists = db.Column(db.Integer)
 
-    squads = db.relationship('Squad', back_populates = 'users', cascade = 'all, delete')
+    # user_players_id = db.Column(db.Integer, db.ForeignKey('user_players.id'), nullable = False)
 
-class UserSchema(ma.Schema):
-    squads = fields.List(fields.Nested('SquadSchema', exclude = ['users']))
+    user_players = db.relationship('UserPlayer', back_populates='players', cascade='all, delete')
+
+
+class PlayerSchema(ma.Schema):
+    user_players = fields.Nested('UserPlayerSchema')
 
     class Meta:
-        fields = ('id', 'name', 'email', 'password', 'is_admin')
+        fields = ('name', 'team', 'position', 'points', 'rebounds', 'assists')
+        ordered = True
