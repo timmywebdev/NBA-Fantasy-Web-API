f9788dec8ed720c7fe5720425edb24133af5ba1f
Timothy Nguyen <timhnguyen98@gmail.com>
2022-11-13 05:34:09 +1100

    added user model

--- src/models/user.py
+++ src/models/user.py
@@ -5,15 +5,16 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(50))
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
-    squads = db.relationship('Squad', back_populates = 'users', cascade = 'all, delete')
+    squads = db.relationship('Squad', back_populates='user', cascade='all, delete')
 
 class UserSchema(ma.Schema):
-    squads = fields.List(fields.Nested('SquadSchema', exclude = ['users']))
+    squads = fields.List(fields.Nested('SquadSchema', exclude=['user']))
 
     class Meta:
         fields = ('id', 'name', 'email', 'password', 'is_admin')
+        ordered = True
