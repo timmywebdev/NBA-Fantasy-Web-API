1441ab9d60eaddfcad1379bfedb15849170f3621
Timothy Nguyen <timhnguyen98@gmail.com>
2022-11-13 20:07:40 +1100

    added validation to user name

--- src/models/user.py
+++ src/models/user.py
@@ -1,11 +1,12 @@
 from init import db, ma
 from marshmallow import fields
+from marshmallow.validate import Length, And, Regexp
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(25))
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
@@ -14,6 +15,13 @@ class User(db.Model):
     squads = db.relationship('Squad', back_populates='user', cascade='all, delete')
 
 class UserSchema(ma.Schema):
+    # Validation for name to be between 3-25 characters and only contain letters and spaces
+    name = fields.String(validate=And(
+        Length(min=3, max=25, error='Username must be between 3 and 25 characters long'),
+        Regexp('^[a-zA-Z ]+$', error='Username can only contain letters and spaces')
+    ))
+    
+    # Excludes user from squads field
     squads = fields.List(fields.Nested('SquadSchema', exclude=['user']))
 
     class Meta:
